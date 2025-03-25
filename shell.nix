8d679ca0356b594cc5454dc7032c16d50f58408d
Aceroph <aos92@proton.me>
2025-03-25 09:40:15 -0400

    Added nix dev environnement

---
+++ shell.nix
@@ -0,0 +1,16 @@
+let
+  pkgs =
+    import
+      (fetchTarball "https://github.com/NixOS/nixpkgs/archive/f0946fa5f1fb876a9dc2e1850d9d3a4e3f914092.tar.gz")
+      { };
+in
+pkgs.mkShell {
+  packages = [
+    (pkgs.python3.withPackages (
+      python-pkgs: with python-pkgs; [
+        discordpy
+        asyncpg
+      ]
+    ))
+  ];
+}
